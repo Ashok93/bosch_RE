20c82be5fcfcde56a2bd94d82fcfe57519d05c99
Ashok M <ashok.kannan93@gmail.com>
2019-06-23 18:18:01 +0200

    Fixed import issue

--- task_1_road_networks/find_shortest_path.py
+++ task_1_road_networks/find_shortest_path.py
@@ -1,45 +1,10 @@
-import json
 import heapq
-from graph_utils import Node, RoadNetworkGraph
+from graph_utils import Node, RoadNetworkGraph, build_road_network_graph
 from plot_utils import visualize_road_network, visualize_shortest_path
-
-def get_road_network_graph(road_network_string):
-    
-    try:
-        road_network_info = json.loads(road_network_string)
-    except ValueError:
-        print("Please check for the proper format of the JSON string provided. \n")
-        raise
-
-    nodes_info = road_network_info["graph"]["nodes"]
-    edges_info = road_network_info["graph"]["edges"]
-    
-    # Create road network graph instance
-    road_network_graph = RoadNetworkGraph()
-
-    # create node instances and add to the road network graph from node information
-    for node_info in nodes_info:
-        node = Node(node_info["id"])
-        road_network_graph.add_node(node)
-    
-    # retrieve and add neighbours of the nodes from edge information
-    for edge_info in edges_info:
-        source_node = road_network_graph.get_node(edge_info["source"])
-        destination_node = road_network_graph.get_node(edge_info["target"])
-        edge_weight = edge_info["weight"]
-        is_directed = edge_info["directed"]
-        orientation = edge_info["orientation"]
-
-        source_node.add_neighbour(destination_node, edge_weight, orientation)
-        if not is_directed:
-            destination_node.add_neighbour(source_node, edge_weight, orientation)
-
-    return road_network_graph
-
 
 def find_shortest_path(road_network_string, start_node_id, destination_node_id):
 
-    road_network_graph = get_road_network_graph(road_network_string)
+    road_network_graph = build_road_network_graph(road_network_string)
     visualize_road_network(road_network_graph)
 
     source_node = road_network_graph.get_node(start_node_id)
@@ -48,7 +13,6 @@ def find_shortest_path(road_network_string, start_node_id, destination_node_id):
     visited_nodes = []
     nodes_queue = []
     heapq.heappush(nodes_queue, (0, source_node))
-    is_destination_reached = False
     
     parent_info = {}
     for node in road_network_graph.nodes:
@@ -56,21 +20,22 @@ def find_shortest_path(road_network_string, start_node_id, destination_node_id):
     
     parent_info[source_node.id]["cost"] = 0
 
-    while len(nodes_queue) and not is_destination_reached:
+    while len(nodes_queue):
         curr_node = heapq.heappop(nodes_queue)[1]
         print(curr_node)
         for neighbour_info in curr_node.neighbours_info:
             curr_neighbour = neighbour_info["node"]
             curr_neighbour_cost = neighbour_info["traversal_weight"]
-            curr_best_cost = parent_info[curr_neighbour.id]["cost"]
-
+            curr_cost = parent_info[curr_neighbour.id]["cost"]
             new_cost = parent_info[curr_node.id]["cost"] + curr_neighbour_cost
-            
-            if new_cost < curr_best_cost:
-                parent_info[curr_neighbour.id] = {"parent_node": curr_node, "cost": new_cost}
-            
+
+            if new_cost < curr_cost:
+                parent_info[curr_neighbour.id]["parent_node"] = curr_node
+                parent_info[curr_neighbour.id]["cost"] = new_cost
+
             if curr_neighbour not in visited_nodes and curr_neighbour not in [n[1] for n in nodes_queue]:
-                heapq.heappush(nodes_queue, (new_cost ,curr_neighbour))
+                cost = new_cost if new_cost < curr_cost else curr_cost
+                heapq.heappush(nodes_queue, (cost ,curr_neighbour))
         
         visited_nodes.append(curr_node)
     
--- task_1_road_networks/graph_utils.py
+++ task_1_road_networks/graph_utils.py
@@ -1,3 +1,5 @@
+import json
+
 class Node:
     def __init__(self, id):
         self.id = id
@@ -5,7 +7,7 @@ class Node:
     
     def add_neighbour(self, neighbour_node, traversal_weight, orientation):
         neighbour_info = {'node': neighbour_node, 
-                          'traversal_weight': traversal_weight.
+                          'traversal_weight': traversal_weight,
                           'orientation': orientation}
         self.neighbours_info.append(neighbour_info)
 
@@ -31,3 +33,37 @@ class RoadNetworkGraph:
         for node in self.nodes:
             graph_str += node.__str__() + "\n"
         return graph_str
+
+
+def build_road_network_graph(road_network_string):
+    
+    try:
+        road_network_info = json.loads(road_network_string)
+    except ValueError:
+        print("Please check for the proper format of the JSON string provided. \n")
+        raise
+
+    nodes_info = road_network_info["graph"]["nodes"]
+    edges_info = road_network_info["graph"]["edges"]
+    
+    # Create road network graph instance
+    road_network_graph = RoadNetworkGraph()
+
+    # create node instances and add to the road network graph from node information
+    for node_info in nodes_info:
+        node = Node(node_info["id"])
+        road_network_graph.add_node(node)
+    
+    # retrieve and add neighbours of the nodes from edge information
+    for edge_info in edges_info:
+        source_node = road_network_graph.get_node(edge_info["source"])
+        destination_node = road_network_graph.get_node(edge_info["target"])
+        edge_weight = edge_info["weight"]
+        is_directed = edge_info["directed"]
+        orientation = edge_info["orientation"]
+
+        source_node.add_neighbour(destination_node, edge_weight, orientation)
+        if not is_directed:
+            destination_node.add_neighbour(source_node, edge_weight, orientation)
+
+    return road_network_graph
